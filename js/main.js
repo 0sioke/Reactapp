f0c0db44c9d848537f0ff2e2e763c45e0cc6b09e
0sioke <143418551+0sioke@users.noreply.github.com>
2024-11-14 14:11:11 +0100

    main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-"use strict";!function(){var n=$("html"),t=function(){$(".btn-menu").on("click",function(t){t.preventDefault(),n.toggleClass("menu-opened")})},e=function(){t()};e()}();
+async function uploadFile(file, presignedUrl) {
+  try {
+    const response = await fetch(presignedUrl, {
+      method: 'PUT',
+      body: file
+    });
+
+    if (!response.ok) {
+      throw new Error(`Upload failed: ${response.statusText}`);
+    }
+
+    console.log('File uploaded successfully!');
+  } catch (error) {
+    console.error('Error uploading file:', error);
+  }
+}
